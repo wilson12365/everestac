de403bec50bc058cdcaf569f0932b3bcad130bdb
Wilson Benitez <wilsonabenitez98@gmail.com>
2020-02-11 18:29:46 -0600

    edit

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ const BlogIndex = ({ data }, location) => {
   return (
     <Layout title={siteTitle}>
       <SEO
-        title="All posts"
+        title="Home"
         keywords={[`blog`, `gatsby`, `javascript`, `react`]}
       />
       {/* <Bio /> */}
